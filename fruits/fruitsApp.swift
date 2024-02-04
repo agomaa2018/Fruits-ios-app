c11dd7a1722ba1486a1bd4260871373a838c5a28
AhmedGomaa <39458565+agomaa2018@users.noreply.github.com>
2024-02-04 11:27:37 +0200

    Initial Commit

---
+++ fruits/fruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruitsApp.swift
+//  fruits
+//
+//  Created by Ahmed Gomaa on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct fruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
